16ad54f276cde3985a7ceddda0998e9c4e3f3956
espindola_esau <esauespindola@gamil.com>
2026-05-21 01:19:36 -0300

    Feat: Estructura inicial limpia de TicketFlow API

---
+++ README.md
@@ -0,0 +1,6 @@
+API REST desarrollada en Node.js y SQLite enfocada en la gestión, soporte y priorización automática de tickets de soporte técnico.
+## Tecnologías utilizadas:
+* **Node.js** (Backend)
+* **Better-SQLite3** (Persistencia de datos ligera)
+* **Dotenv** (Variables de entorno)
+
---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+
+
+app.get('/api/ping', (req, res) => {
+    res.json({ message: "Servidor operativo, listo para los tickets" });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo impecable en el puerto ${PORT}`);
+});
