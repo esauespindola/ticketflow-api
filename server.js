77215da96296c1159d9478de1158efe96315ad77
espindola_esau <esauespindola@gamil.com>
2026-05-23 21:42:58 -0300

    fix: mover cors al lugar correcto en server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const db = require('./db');
-const cors = require('cors')
-app.use(cors())
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.json());
 
